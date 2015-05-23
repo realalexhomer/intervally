a091bcbed064f2d72393b94ca10879f915a88534
cpachomski <cpachomski@gmail.com>
2015-05-23 16:21:30 -0400

    add pseudocode

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// DOM selection and events for playing
+
+
+
+
+//Steps
+//1. generate audio context upon page load
+//2. create two audio objects with different frequencies
+//3. add play/pause button for both pitches
+//4. add change frequency box that changes frequency upon button press
+
+$( document ).ready(function(){
+  console.log('spagoo')
+});
