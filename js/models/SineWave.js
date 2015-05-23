f1b50063a5d547ba8f08dc416453df116c530fd5
cpachomski <cpachomski@gmail.com>
2015-05-23 15:22:32 -0400

    add sinewave model

---
+++ js/models/SineWave.js
@@ -0,0 +1,18 @@
+SineWave = function(context) {
+  var that = this;
+  this.x = 0; // initial samples
+  this.context = context;
+  this.node = context.createScriptProcessor(1024, 1, 1);
+  this.node.onaudioprocess = function(event) { that.process(event) };
+
+}
+
+// Processess wave and outputs to
+
+SineWave.prototype.process = function(event) {
+  var data = event.outputBuffer.getChannelData(0);
+  for (var i = 0; i < data.length; ++i){
+    data[i] = Math.sin(this.x++);
+  }
+}
+
