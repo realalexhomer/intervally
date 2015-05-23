68708e276c35cacb9ec3252efebbe10d3db898f3
cpachomski <cpachomski@gmail.com>
2015-05-23 18:31:13 -0400

    fix test dictionary

--- js/models/interval-dicionaries.js
+++ js/models/interval-dicionaries.js
@@ -1,14 +1,14 @@
 var cMajor = {
-  'min2' : {'base': 523.25 , 'diff': 554.37},
-  'maj2' : {'base': 523.25 , 'diff': 587.33},
-  'min3' : {'base': 523.25 , 'diff': 622.25},
-  'maj3' : {'base': 523.25 , 'diff': 659.25},
-  'perf4' : {'base': 523.25 , 'diff': 698.46},
-  'dim5' : {'base': 523.25 , 'diff': 739.99},
-  'perf5': {'base': 523.25 , 'diff': 783.99},
-  'min6' : {'base': 523.25 , 'diff': 830.61},
-  'maj6' : {'base': 523.25 , 'diff': 880.00},
-  'min7' : {'base': 523.25 , 'diff': 932.33},
-  'maj7' : {'base': 523.25 , 'diff': 987.77},
-  'perf8': {'base': 523.25 , 'diff': 1046.50}
+  min2 : {base: { freq: 523.25, move_to: n12 }, diff: {freq: 554.37, move_to: n11}},
+  maj2 : {base: { freq: 523.25, move_to: n12 }, diff: { freq: 587.33, move_to: n10}},
+  min3 : {base: { freq: 523.25, move_to: n12 }, diff: { freq: 622.25, move_to: n9}},
+  maj3 : {base: { freq: 523.25, move_to: n12 }, diff: { freq: 659.25, move_to: n8}},
+  perf4 : {base: { freq: 523.25, move_to: n12 }, diff: { freq: 698.46, move_to: n7}},
+  dim5 : {base: { freq: 523.25, move_to: n12 }, diff: { freq: 739.99, move_to: n6}},
+  perf5: {base: { freq: 523.25, move_to: n12 }, diff: { freq: 783.99, move_to: n5}},
+  min6 : {base: { freq: 523.25, move_to: n12 }, diff: { freq: 830.61, move_to: n4}},
+  maj6 : {base: { freq: 523.25, move_to: n12 }, diff: { freq: 880.00, move_to: n3}},
+  min7 : {base: { freq: 523.25, move_to: n12 }, diff: { freq: 932.33, move_to: n3}},
+  maj7 : {base: { freq: 523.25, move_to: n12 }, diff: { freq: 987.77, move_to: n2}},
+  perf8: {base: { freq: 523.25, move_to: n12 }, diff: { freq: 1046.50, move_to: n1}}
 }
