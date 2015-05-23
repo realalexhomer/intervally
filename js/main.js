18081b141f0c4c7f44a0a9bba81a0c368cd61692
cpachomski <cpachomski@gmail.com>
2015-05-23 17:38:00 -0400

    remove pseudocode'

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 // DOM selection and events for playing
 
 
-
-
-//Steps
-//1. generate audio context upon page load
-//2. create two audio objects with different frequencies
-//3. add play/pause button for both pitches
-//4. add change frequency box that changes frequency upon button press
-//5. add change amplitude box that changes amplitude upon button press
-
 $( document ).ready(function(){
 
   //generate audio context for each sinewave object
