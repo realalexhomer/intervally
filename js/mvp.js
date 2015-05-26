babe0efd4025984e202282cd304030b832dc6ba9
realalexhomer <realalexhomer@gmail.com>
2015-05-26 17:10:17 -0400

    finish mvp where dragging corresponds to freq

--- js/mvp.js
+++ js/mvp.js
@@ -1,17 +1,13 @@
 $('document').ready(function(){
 
-  //Y AND X MARGINS ARE SET TO 50!!!
-
   var s               = Snap('#main'),
       waves           = {},
       lineHeight      = 10,
       lineColor       = '#D8D8D8',
       audioContext    = new AudioContext();
 
-
-
   svgFromKey(keyOfC);
-
+  s.addClass('goo');
 
   function svgFromKey(freqArr){
     var keyObj = makeKeyObj(freqArr);
@@ -23,21 +19,8 @@ $('document').ready(function(){
       size: 40, 
       fill:'green'
     });
-    // drawCircle({
-    //   x: 500,
-    //   y: keyObj.lines.line7.el.attr("y"),
-    //   size: 40,
-    //   fill:'pink'
-    // });
-    makeWave(test);
     test.drag()
-    // changeWaveOnDrag(test, waves[test.name], 'cy', SineWave.prototype.setFrequency);
-    // console.log(keyObj);
-    changeFreqOnDrag(test, waves[test.name], keyObj);
-    // test.drag(function(){
-    //   var bb = this.getBBox();
-    //   console.log(bb);
-    // })
+    makeWave(test, keyObj);
   }
 
   function makeKeyObj(freqArr){
@@ -77,79 +60,31 @@ $('document').ready(function(){
           h       = lineHeight;
       keyObj.lines[lineKey].el = s.rect(x,y,w,h);
       keyObj.lines[lineKey].el.attr('fill', lineColor);
-      console.log(y);
     }
-    console.log(keyObj.lines);
   }
 
   function drawCircle(a){
     var circle = s.circle(a.x, a.y, a.size);
     circle.attr('fill', a.fill);
     circle.name = 'circle1'
-    // circle.drag();
-    console.log(circle);
     return circle;
   }
 
-  function makeWave(circle){
+  function makeWave(circle, keyObj){
     if (!waves[circle.name]){
       waves[circle.name] = new SineWave(audioContext);
-      waves[circle.name].setFrequency(circle.attr('cy'));
+      changeFreqOnDrag(circle, waves[circle.name], keyObj);
       waves[circle.name].play();
     }else{
       throw 'circle already has a wave - cannot set more than one wave for a circle.'
     }
   }
 
-  function changeWaveOnDrag(obj, wave, objAttr, waveFunc){
-    obj.drag(function(){
-      obj.bb = obj.getBBox();
-      waveFunc.call(wave, obj.bb[objAttr])
-    })
-  }
-
   function changeFreqOnDrag(obj, wave, keyObj){
     obj.drag(function(){
       obj.bb = obj.getBBox();
-      var y = obj.bb.cy;
-      console.log(y);
-      // var freq = obj.bb.cy / keyObj.ratio
-      // wave.setFrequency(freq);
-      // // console.log(, keyObj.ratio)
-      // console.log(keyObj);
-      // // wave.setFrequency(obj.bb / keyObj.ratio)
-      // console.log(wave);
-
+      var freq = keyObj.high - (obj.bb.cy / keyObj.ratio);
+      wave.setFrequency(freq);
     })
   }
-
-  //
-  
-
-  function findFreq(keyObj, y){
-    return y / keyObj.ratio;
-  }
-
-  function setElFreq(wave, y, ratio){
-    wave.setFrequency(y/ratio);
-  }
-
-  function assignFreq(el, keyObj){
-    // el.drag(setElFreq())
-
-  }
-
-
-
-
-  // draw lines from key
-
-  // draw blobs
-
-  // play sounds in relation to key
-
-
-
-
-
 });
